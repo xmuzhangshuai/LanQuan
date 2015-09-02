8a09f7f1f247da64c387f1b218e1f844bdad88b7
zhangshuai <zhangshuai@zhangshuai-pc>
2015-09-02 21:22:37 +0800

    创建频道时弹出提示框
    Signed-off-by: zhangshuai <zhangshuai@zhangshuai-pc>

--- lanquan/src/com/lanquan/ui/CreatChannelChooseActivity.java
+++ lanquan/src/com/lanquan/ui/CreatChannelChooseActivity.java
@@ -1,5 +1,7 @@
 package com.lanquan.ui;
 
+import org.apache.http.Header;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,12 @@ import android.widget.ImageView;
 
 import com.lanquan.R;
 import com.lanquan.base.BaseActivity;
+import com.lanquan.customwidget.MyAlertDialog;
+import com.lanquan.utils.AsyncHttpClientTool;
+import com.lanquan.utils.JsonTool;
+import com.lanquan.utils.LogTool;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
 
 /** 
  * 类描述 ：选择创建图文频道、文字频道和打卡频道的页面
@@ -58,27 +66,20 @@ public class CreatChannelChooseActivity extends BaseActivity implements OnClickL
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		Intent intent = new Intent(CreatChannelChooseActivity.this, CreatChannelActivity.class);
 		switch (v.getId()) {
 		case R.id.close_btn:
 			finish();
 			break;
 		case R.id.photo_btn:
-			intent.putExtra(CreatChannelActivity.CHANNAL_TYPE, "0");
-			startActivity(intent);
-			overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+			showCreateDialog(0);
 			break;
 
 		case R.id.txt_btn:
-			intent.putExtra(CreatChannelActivity.CHANNAL_TYPE, "1");
-			startActivity(intent);
-			overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+			showCreateDialog(1);
 			break;
 
 		case R.id.punch_btn:
-			intent.putExtra(CreatChannelActivity.CHANNAL_TYPE, "2");
-			startActivity(intent);
-			overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+			showCreateDialog(2);
 			break;
 
 		default:
@@ -87,4 +88,45 @@ public class CreatChannelChooseActivity extends BaseActivity implements OnClickL
 
 	}
 
+	// 传递评论Id,用于删除评论（如果是自己发表的）
+	public void showCreateDialog(final int type) {
+		final MyAlertDialog dialog = new MyAlertDialog(CreatChannelChooseActivity.this);
+		if (type == 0) {
+			dialog.setTitle("你将创建的是纯文字频道");
+			dialog.setMessage("在该频道里只允许发文字，频道 一旦创建，属性不能修改，你确定吗？");
+		} else if (type == 1) {
+			dialog.setTitle("你将创建的是图文频道");
+			dialog.setMessage("在该频道里你可以同时发图片和文字，也只发文字或者图片，频道一旦创建，属性不能修改，你确定吗？");
+		} else if (type == 2) {
+			dialog.setTitle("你将创建的是打卡频道");
+			dialog.setMessage("在该频道里可以记录打卡，频道一旦创建，属性不能修改，你确定吗？");
+		} else {
+			return;
+		}
+
+		View.OnClickListener comfirm = new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				dialog.dismiss();
+				Intent intent = new Intent(CreatChannelChooseActivity.this, CreatChannelActivity.class);
+				intent.putExtra(CreatChannelActivity.CHANNAL_TYPE, "" + type);
+				startActivity(intent);
+				overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+				finish();
+			}
+		};
+		View.OnClickListener cancle = new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				dialog.dismiss();
+			}
+		};
+		dialog.setPositiveButton("确定", comfirm);
+		dialog.setNegativeButton("取消", cancle);
+		dialog.show();
+	}
 }
