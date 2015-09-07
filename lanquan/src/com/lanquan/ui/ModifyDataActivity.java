4e3a3303324c5bcdd0ec28a44e1f3e184ec9395b
zhangshuai <1207509737@qq.com>
2015-09-07 12:56:51 +0800

    增加修改头像功能
    Signed-off-by: zhangshuai <1207509737@qq.com>

--- lanquan/src/com/lanquan/ui/ModifyDataActivity.java
+++ lanquan/src/com/lanquan/ui/ModifyDataActivity.java
@@ -40,15 +40,13 @@ import com.lanquan.utils.UserPreference;
 import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.TextHttpResponseHandler;
 
-/** 
- * 类描述 ：修改资料
- * 类名： ModifyDataActivity.java  
- * Copyright:   Copyright (c)2015    
- * Company:     zhangshuai   
- * @author:     zhangshuai    
- * @version:    1.0    
- * 创建时间:    2015-8-14 上午9:57:13  
-*/
+/**
+ * 类描述 ：修改资料 类名： ModifyDataActivity.java Copyright: Copyright (c)2015 Company:
+ * zhangshuai
+ * 
+ * @author: zhangshuai
+ * @version: 1.0 创建时间: 2015-8-14 上午9:57:13
+ */
 public class ModifyDataActivity extends BaseFragmentActivity implements OnClickListener {
 	private TextView leftTextView;// 导航栏左侧文字
 	private View leftButton;// 导航栏左侧按钮
@@ -56,15 +54,15 @@ public class ModifyDataActivity extends BaseFragmentActivity implements OnClickL
 	private ImageView cameraImage;
 	private View changeHeadImageBtn;
 
-	/***昵称***/
+	/*** 昵称 ***/
 	private View nicknameView;
 	private TextView nickNameTextView;
 
-	/***修改密码***/
+	/*** 修改密码 ***/
 	private View passView;
 	private TextView passTextView;
 
-	/***更换手机号***/
+	/*** 更换手机号 ***/
 	private View phoneView;
 	private TextView phoneTextView;
 
@@ -146,8 +144,7 @@ public class ModifyDataActivity extends BaseFragmentActivity implements OnClickL
 						String fileName;
 						// 删除上一次截图的临时文件
 						SharedPreferences sharedPreferences = getSharedPreferences("temp", Context.MODE_WORLD_WRITEABLE);
-						ImageTools.deletePhotoAtPathAndName(Environment.getExternalStorageDirectory().getAbsolutePath(),
-								sharedPreferences.getString("crop_tempName", ""));
+						ImageTools.deletePhotoAtPathAndName(Environment.getExternalStorageDirectory().getAbsolutePath(), sharedPreferences.getString("crop_tempName", ""));
 
 						// 保存本次截图临时文件名字
 						fileName = String.valueOf(System.currentTimeMillis()) + ".jpg";
@@ -155,8 +152,7 @@ public class ModifyDataActivity extends BaseFragmentActivity implements OnClickL
 						editor.putString("crop_tempName", fileName);
 						editor.commit();
 
-						uploadImage(ImageTools.savePhotoToSDCard(photo, Environment.getExternalStorageDirectory().getAbsolutePath(), fileName, 100)
-								.getAbsolutePath());
+						uploadImage(ImageTools.savePhotoToSDCard(photo, Environment.getExternalStorageDirectory().getAbsolutePath(), fileName, 100).getAbsolutePath());
 						LogTool.e("裁剪返回" + "Bitmap");
 					}
 				}
@@ -202,6 +198,7 @@ public class ModifyDataActivity extends BaseFragmentActivity implements OnClickL
 
 	/**
 	 * 上传头像
+	 * 
 	 * @param filePath
 	 */
 	public void uploadImage(final String imageUrl) {
@@ -228,6 +225,7 @@ public class ModifyDataActivity extends BaseFragmentActivity implements OnClickL
 							try {
 								userPreference.setU_avatar(jsonObject.getString("url"));
 								imageLoader.displayImage(userPreference.getU_avatar(), headImageView, ImageLoaderTool.getCircleHeadImageOptions());
+								modifyHeadImage(jsonObject.getString("url"));
 							} catch (JSONException e) {
 								// TODO Auto-generated catch block
 								e.printStackTrace();
@@ -252,15 +250,44 @@ public class ModifyDataActivity extends BaseFragmentActivity implements OnClickL
 		} else {
 			LogTool.e("本地文件为空");
 		}
-
 	}
 
 	/**
-	* 显示对话框，从拍照和相册选择图片来源
-	* 
-	* @param context
-	* @param isCrop
-	*/
+	 * 修改头像
+	 */
+	private void modifyHeadImage(String url) {
+		RequestParams params = new RequestParams();
+		params.put("avatar", url);
+		params.put("access_token", userPreference.getAccess_token());
+		TextHttpResponseHandler responseHandler = new TextHttpResponseHandler() {
+			@Override
+			public void onSuccess(int statusCode, Header[] headers, String response) {
+				// TODO Auto-generated method stub
+				LogTool.i("" + statusCode + response);
+				JsonTool jsonTool = new JsonTool(response);
+				String status = jsonTool.getStatus();
+				if (status.equals(JsonTool.STATUS_SUCCESS)) {
+					LogTool.i(jsonTool.getMessage());
+				} else if (status.equals(JsonTool.STATUS_FAIL)) {
+					LogTool.e(jsonTool.getMessage());
+				}
+			}
+
+			@Override
+			public void onFailure(int statusCode, Header[] headers, String errorResponse, Throwable e) {
+				// TODO Auto-generated method stub
+				LogTool.e("头像上传失败！" + errorResponse);
+			}
+		};
+		AsyncHttpClientTool.post("api/user/modifyAvatar", params, responseHandler);
+	}
+
+	/**
+	 * 显示对话框，从拍照和相册选择图片来源
+	 * 
+	 * @param context
+	 * @param isCrop
+	 */
 	private void showPicturePicker(Context context) {
 
 		final MyMenuDialog myMenuDialog = new MyMenuDialog(ModifyDataActivity.this);
@@ -286,8 +313,7 @@ public class ModifyDataActivity extends BaseFragmentActivity implements OnClickL
 
 					// 删除上一次截图的临时文件
 					SharedPreferences sharedPreferences = getSharedPreferences("temp", Context.MODE_WORLD_WRITEABLE);
-					ImageTools.deletePhotoAtPathAndName(Environment.getExternalStorageDirectory().getAbsolutePath(),
-							sharedPreferences.getString("tempName", ""));
+					ImageTools.deletePhotoAtPathAndName(Environment.getExternalStorageDirectory().getAbsolutePath(), sharedPreferences.getString("tempName", ""));
 
 					// 保存本次截图临时文件名字
 					fileName = String.valueOf(System.currentTimeMillis()) + ".jpg";
