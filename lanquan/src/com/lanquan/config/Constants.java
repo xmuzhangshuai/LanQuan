30d02899c43feb4e6ffbb6a6bc8fade224d1fb7f
zhangshuai <1207509737@qq.com>
2015-10-09 18:34:10 +0800

    Signed-off-by: zhangshuai <1207509737@qq.com>

--- lanquan/src/com/lanquan/config/Constants.java
+++ lanquan/src/com/lanquan/config/Constants.java
@@ -1,14 +1,9 @@
 package com.lanquan.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Constants {
 
 	// 包名
-	public static final String PACKAGENAME = "com.quanzi";
-
-	// public static String testIP = "http://192.168.1.101/";
+	public static final String PACKAGENAME = "com.lanquan";
 
 	// 域名或者是IP(旧)
 	public static String AppliactionServerIP = "http://120.24.183.74";
@@ -17,15 +12,15 @@ public class Constants {
 
 	public static String AppliactionServerIP_Share = "http://120.25.62.195";
 	// 域名或者是IP
-//	public static String AppliactionServerIP = "http://120.25.62.195/";
-//
-//	public static String AppliactionServerDomain = "http://120.25.62.195/index.php/";
+	//	public static String AppliactionServerIP = "http://120.25.62.195/";
+	//
+	//	public static String AppliactionServerDomain = "http://120.25.62.195/index.php/";
 
 	public static String SignKey = "dfeb3d35bc3543rdc234";
 
 	public static class Config {
 		// 是否处于开发模式
-		public static final boolean DEVELOPER_MODE = true;
+		public static final boolean DEVELOPER_MODE = false;
 
 		// 接受验证码时间为120s
 		public static int AUTN_CODE_TIME = 120;
@@ -43,78 +38,6 @@ public class Constants {
 
 		// 聊天每次刷新纪录条数
 		public static int LOAD_MESSAGE_COUNT = 20;
-	}
-
-	// 情感状态
-	public static class LoveState {
-		// 单身
-		public static final String SINGLE = "single";
-		// 恋爱
-		public static final String INLOVE = "in_love";
-		// 已婚
-		public static final String MARRIED = "married";
-	}
-
-	// 评论类型
-	public static class CommentType {
-		// 评论
-		public static final String COMMENT = "COMMENT";
-		// 回复
-		public static final String REPLY = "REPLY";
-	}
-
-	// 身份
-	public static class Identity {
-		// 学生
-		public static final String STUDENT = "student";
-		// 校友
-		public static final String SCHOOLMATE = "schoolmate";
-	}
-
-	// 审核状态
-	public static class VertifyState {
-		// 通过
-		public static final int PASSED = 2;
-		// 正在审核
-		public static final int VERTIFING = 1;
-		// 未通过
-		public static final int NOTPASSED = -1;
-		// 未提交
-		public static final int NOTSUBMIT = 0;
-	}
-
-	// 活动种类
-	public static class ActivityType {
-		public static final String JIANGZUO = "讲座";
-		public static final String ZHAOPIN = "招聘";
-		public static final String SHETUAN = "社团";
-		public static final String YINYUE = "音乐";
-		public static final String DIANYING = "电影";
-		public static final String YUNDONG = "运动";
-		public static final String JUHUI = "聚会";
-		public static final String ZHANLAN = "展览";
-		public static final String GONGYI = "公益";
-		public static final String YOUHUIQUAN = "优惠券";
-
-		public static List<String> getList() {
-			List<String> temp = new ArrayList<String>();
-			temp.add(JIANGZUO);
-			temp.add(ZHAOPIN);
-			temp.add(SHETUAN);
-			temp.add(YINYUE);
-			temp.add(DIANYING);
-			temp.add(YUNDONG);
-			temp.add(JUHUI);
-			temp.add(ZHANLAN);
-			temp.add(GONGYI);
-			temp.add(YOUHUIQUAN);
-			return temp;
-		}
-	}
-
-	public static class BaiduPushConfig {
-		public final static String API_KEY = "bcB8dCkZh0GVtMTdpyTTabj3";
-		public final static String SECRIT_KEY = "y75ge4lbEEGth1nwbTveiGr7yHARKnm2";
 	}
 
 	public static class WeChatConfig {
@@ -135,11 +58,6 @@ public class Constants {
 	public static class Extra {
 		public static final String IMAGES = "com.nostra13.example.universalimageloader.IMAGES";
 		public static final String IMAGE_POSITION = "com.nostra13.example.universalimageloader.IMAGE_POSITION";
-	}
-
-	public static class Gender {
-		public static final String MALE = "男";
-		public static final String FEMALE = "女";
 	}
 
 }
